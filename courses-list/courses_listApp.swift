a4f01a497db35c3baf17fa430a9a153f9d3c8df7
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-12 13:16:29 -0600

    Initial Commit

---
+++ courses-list/courses_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  courses_listApp.swift
+//  courses-list
+//
+//  Created by Jorge on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct courses_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
